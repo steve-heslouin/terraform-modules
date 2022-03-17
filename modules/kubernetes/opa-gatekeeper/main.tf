de8c0a6402bf69381e8db69137b247de60142e57
Steve Heslouin <50872822+steve-heslouin@users.noreply.github.com>
2022-03-17 17:22:10 +0100

    Update main.tf

--- modules/kubernetes/opa-gatekeeper/main.tf
+++ modules/kubernetes/opa-gatekeeper/main.tf
@@ -87,11 +87,11 @@
 terraform {
   required_version = "1.1.5"
 
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.8.0"
-    }
+#   required_providers {
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "2.8.0"
+#     }
     helm = {
       source  = "hashicorp/helm"
       version = "2.4.1"
